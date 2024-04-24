1dfa09db04e20437e6591da066255c930ad11e51
sahir <sahir@pop-os.localdomain>
2024-04-24 14:32:24 +0530

    fix: typescript support added

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import './App.css';
+import { DemoHome } from './DemoHome';
+
+function App() {
+  return (
+    <div className="App">
+      <DemoHome />
+    </div>
+  );
+}
+
+export default App;
